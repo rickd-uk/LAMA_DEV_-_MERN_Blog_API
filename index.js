48e43fbdc4b6a467b3f9e8e14c8f66953c868021
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2021-09-19 13:27:49 +0900

    Add Upload file Capability

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const userRoute = require('./routes/users')
 const postRoute = require('./routes/posts')
 const categoryRoute = require('./routes/categories')
 
+const multer = require('multer')
+
 dotenv.config()
 app.use(express.json())
 
@@ -19,6 +21,20 @@ mongoose
   })
   .then(console.log('Connected to mongoDB'))
   .catch((err) => console.log(err))
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images')
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name)
+  },
+})
+
+const upload = multer({ storage: storage })
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  res.json('File has been uploaded')
+})
 
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
